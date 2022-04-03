722e06549603eb92bd82df8c21d8c2136cbc275b
larscom <larskniep@gmail.com>
2022-04-03 21:52:28 +0200

    add conditional accounts render

--- src/pages/Accounts.tsx
+++ src/pages/Accounts.tsx
@@ -1,38 +1,64 @@
 import { IAccount } from '$models/account';
-import { Group, ScrollArea, Title } from '@mantine/core';
+import { Button, createStyles, Group, ScrollArea, Text, Title } from '@mantine/core';
 import { useLocalStorage } from '@mantine/hooks';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AccountsList from '../components/Accounts/AccountsList';
 import AddAccountButton from '../components/Accounts/AddAccountButton';
 import DeleteAccountsButton from '../components/Accounts/DeleteAccountsButton';
 import SearchAccount from '../components/Accounts/SearchAccount';
 
+const useStyles = createStyles((theme) => ({
+  actions: {
+    paddingLeft: theme.spacing.xs,
+    paddingRight: theme.spacing.xs
+  }
+}));
+
 function Accounts() {
   const [searchTerm, setSearchTherm] = useState('');
+
   const [accounts, setAccounts] = useLocalStorage<IAccount[]>({
     key: 'accounts',
     defaultValue: []
   });
 
+  const { classes } = useStyles();
+
+  const navigate = useNavigate();
+
+  const renderNoAccounts = () => {
+    return (
+      <>
+        <Text>You don't have any accounts yet.</Text>
+        <Group>
+          <Button onClick={() => navigate('add')}>Create account</Button>
+        </Group>
+      </>
+    );
+  };
+
+  const renderAccounts = () => {
+    return (
+      <>
+        <Group className={classes.actions} position="apart">
+          <SearchAccount total={accounts.length} onSearch={setSearchTherm}></SearchAccount>
+          <Group>
+            <AddAccountButton />
+            <DeleteAccountsButton setAccounts={setAccounts} total={accounts.length} />
+          </Group>
+        </Group>
+        <ScrollArea offsetScrollbars style={{ height: '70vh' }}>
+          <AccountsList searchTerm={searchTerm} accounts={accounts} />
+        </ScrollArea>
+      </>
+    );
+  };
+
   return (
     <>
       <Title order={2}>Accounts</Title>
-      <Group
-        sx={(theme) => ({
-          paddingLeft: theme.spacing.xs,
-          paddingRight: theme.spacing.xs
-        })}
-        position="apart"
-      >
-        <SearchAccount total={accounts.length} onSearch={setSearchTherm}></SearchAccount>
-        <Group>
-          <AddAccountButton />
-          <DeleteAccountsButton setAccounts={setAccounts} total={accounts.length} />
-        </Group>
-      </Group>
-      <ScrollArea offsetScrollbars style={{ height: '70vh' }}>
-        <AccountsList searchTerm={searchTerm} accounts={accounts} />
-      </ScrollArea>
+      {accounts.length ? renderAccounts() : renderNoAccounts()}
     </>
   );
 }
