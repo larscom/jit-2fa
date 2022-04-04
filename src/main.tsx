001dd76b3d15a405c9cc3027a878ebf5f85aca2f
larscom <larskniep@gmail.com>
2022-04-04 09:52:52 +0200

    add fb

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,20 @@
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+getAnalytics(
+  initializeApp({
+    apiKey: 'AIzaSyAMKlJYmXlRTGmMCXJso0KYHqZCX5cAYW0',
+    authDomain: 'jit-2fa.firebaseapp.com',
+    projectId: 'jit-2fa',
+    storageBucket: 'jit-2fa.appspot.com',
+    messagingSenderId: '735249302962',
+    appId: '1:735249302962:web:15ca2c6a38ae8e28b510ab',
+    measurementId: 'G-J35KQWC200'
+  })
+);
 
 ReactDOM.render(
   <React.StrictMode>
