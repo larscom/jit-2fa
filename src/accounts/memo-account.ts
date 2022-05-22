29dc9b6d219a9a32ade5036856502886fdfc7498
larscom <larskniep@gmail.com>
2022-05-22 23:00:34 +0200

    fix lint error

--- src/accounts/memo-account.ts
+++ src/accounts/memo-account.ts
@@ -5,5 +5,6 @@ interface PropsWithAccount {
   account: IAccount;
 }
 
+/* eslint-disable react/display-name */
 export const memoAccount = <T extends PropsWithAccount>(component: FunctionComponent<T>) =>
   memo(component, ({ account: prevAccount }, { account: nextAccount }) => prevAccount.uuid === nextAccount.uuid);
